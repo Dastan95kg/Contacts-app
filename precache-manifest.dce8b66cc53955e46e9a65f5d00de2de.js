ba56eefb743c50c9a3e255814dccc1dd450bbdc5
Dastan95kg <dastan.maratov95@gmail.com>
2020-04-18 15:28:18 +0600

    Updates

---
+++ precache-manifest.dce8b66cc53955e46e9a65f5d00de2de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5654d1bd9fe2034629549f9f3afce3e0",
+    "url": "/Contacts-app/index.html"
+  },
+  {
+    "revision": "77bacdcfacd89b8d6ba5",
+    "url": "/Contacts-app/static/css/main.84da188b.chunk.css"
+  },
+  {
+    "revision": "041e2c9219f445e55c14",
+    "url": "/Contacts-app/static/js/2.dba9fd64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Contacts-app/static/js/2.dba9fd64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77bacdcfacd89b8d6ba5",
+    "url": "/Contacts-app/static/js/main.6e80ae07.chunk.js"
+  },
+  {
+    "revision": "adbf40f28e771192d4a5",
+    "url": "/Contacts-app/static/js/runtime-main.b969ba7b.js"
+  },
+  {
+    "revision": "c51ab98fe8e7dd99d45784ca62e8406c",
+    "url": "/Contacts-app/static/media/avatar.c51ab98f.png"
+  },
+  {
+    "revision": "6448ca1d60880d4b4d79ddf8daf3b745",
+    "url": "/Contacts-app/static/media/item-ava.6448ca1d.png"
+  }
+]);
